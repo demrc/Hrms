220a470889a5e501a3739c200fcbc57cd9f03c3c
DESKTOP-0LUNRAG\ED <demrc.emre@gmail.com>
2021-06-09 03:03:38 +0300

    updated

--- hrms/src/main/java/hrms/hrms/business/concretes/SchoolManager.java
+++ hrms/src/main/java/hrms/hrms/business/concretes/SchoolManager.java
@@ -1,5 +1,63 @@
 package hrms.hrms.business.concretes;
 
-public class SchoolManager {
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import hrms.hrms.business.abstacts.SchoolService;
+import hrms.hrms.core.utilities.results.DataResult;
+import hrms.hrms.core.utilities.results.Result;
+import hrms.hrms.core.utilities.results.SuccessDataResult;
+import hrms.hrms.core.utilities.results.SuccessResult;
+import hrms.hrms.dataAccess.abstracts.SchoolDao;
+import hrms.hrms.entities.concretes.School;
+
+@Service
+public class SchoolManager implements SchoolService {
+
+	private SchoolDao schoolDao;
+	@Autowired
+	public SchoolManager(SchoolService schoolService) {
+		super();
+		this.schoolDao=schoolDao;
+	}
+	
+	@Override
+	public Result add(School school) {
+		this.schoolDao.save(school);
+		return new SuccessResult();
+	}
+
+	@Override
+	public Result update(School school) {
+		this.schoolDao.save(school);
+		return new SuccessResult();
+	}
+
+	@Override
+	public Result delete(int id) {
+		this.schoolDao.delete(null);
+		return null;
+	}
+
+	@Override
+	public DataResult<List<School>> getAll() {
+		return new SuccessDataResult<List<School>>
+		(this.schoolDao.findAll(),"Data listed");
+	}
+
+	@Override
+	public DataResult<School> getById(int id) {
+		return new SuccessDataResult<School>
+		(this.schoolDao.getById(id),"Data listed");
+	}
+
+	@Override
+	public DataResult<List<School>> getByDate(Date finishDate) {
+		return new SuccessDataResult<List<School>>
+		(this.schoolDao.getByDate(finishDate),"Data listed");
+	}
 
 }
