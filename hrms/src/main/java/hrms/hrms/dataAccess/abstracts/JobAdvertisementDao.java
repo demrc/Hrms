058a665f1d3dac208e8e137dbe25776f3423dee5
DESKTOP-0LUNRAG\ED <demrc.emre@gmail.com>
2021-06-11 21:30:33 +0300

    updated

--- hrms/src/main/java/hrms/hrms/dataAccess/abstracts/JobAdvertisementDao.java
+++ hrms/src/main/java/hrms/hrms/dataAccess/abstracts/JobAdvertisementDao.java
@@ -4,8 +4,10 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import hrms.hrms.entities.concretes.JobAdvertisement;
+import hrms.hrms.entities.dtos.JobAdvertisementWithTools;
 
 public interface JobAdvertisementDao extends JpaRepository<JobAdvertisement, Integer> {
 
@@ -15,5 +17,9 @@ public interface JobAdvertisementDao extends JpaRepository<JobAdvertisement, Int
 	List<JobAdvertisement> getByDeadLine(Date deadline);
 	List<JobAdvertisement> getByActiveTrueAndJobName(String name);
 	List<JobAdvertisement> getByActiveTrueAndOpenPositions(int open_positions);
+	
+	@Query("Select new hrms.hrms.entities.dtos.JobAdvertisementWithTools(j.id,c.cityName,j.jobAdName)"
+			+ "From City c Inner Join c.job_advertisements j")
+	List<JobAdvertisementWithTools> getJobAdDetails();
 	
 }
--- hrms/src/main/java/hrms/hrms/entities/dtos/JobAdvertisementWithTools.java
+++ hrms/src/main/java/hrms/hrms/entities/dtos/JobAdvertisementWithTools.java
@@ -1,5 +1,41 @@
 package hrms.hrms.entities.dtos;
 
 public class JobAdvertisementWithTools {
+	private int id;
+	private String cityName;
+	private String jobAdName;
+	
+	public JobAdvertisementWithTools() {}
+
+	public JobAdvertisementWithTools(int id, String cityName, String jobAdName) {
+		super();
+		this.id = id;
+		this.cityName = cityName;
+		this.jobAdName = jobAdName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public String getJobAdName() {
+		return jobAdName;
+	}
+
+	public void setJobAdName(String jobAdName) {
+		this.jobAdName = jobAdName;
+	}
 
 }
